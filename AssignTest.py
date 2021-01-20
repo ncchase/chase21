11ab2bbc9ffa83d6cf4d6fb093163df50d6dab04
coconuts <coconutswastaken@gmail.com>
2021-01-20 14:04:39 +1300

    Create AssignTest.py
    First commit - assign test file - not working yet.

---
+++ AssignTest.py
@@ -0,0 +1,20 @@
+import random
+
+form_class_num = int(input("How many forms"))
+students = []
+
+
+for x in range(form_class_num):
+
+    new_form = []
+    
+    form_length = int(input("How many students in form", x))
+
+    for y in range(form_length):
+
+        new_form.append(int((str(x) + str(y))
+
+    students.append(new_form)
+
+print(students)
+
