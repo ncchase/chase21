1bd683ad0cc3bb2800166055165833fa70c2001d
coconuts <coconutswastaken@gmail.com>
2021-01-20 14:10:39 +1300

    Update AssignTest.py

--- AssignTest.py
+++ AssignTest.py
@@ -1,6 +1,6 @@
 import random
 
-form_class_num = int(input("How many forms"))
+form_class_num = int(input("How many forms: "))
 students = []
 
 
@@ -8,12 +8,12 @@ for x in range(form_class_num):
 
     new_form = []
     
-    form_length = int(input("How many students in form", x))
+    form_length = int(input(str("How many students in form ") + str(x) + ": "))
 
     for y in range(form_length):
 
-        new_form.append(int((str(x) + str(y))
-
+        new_form.append(int((str(x) + str(y))))
+    
     students.append(new_form)
 
 print(students)
