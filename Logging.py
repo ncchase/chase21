dd1b5e724801c5ab5c8ec2f87fe318a28614c954
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-03-10 12:56:40 +1300

    Prepare functions for logging

--- Logging.py
+++ Logging.py
@@ -1,11 +1,23 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
+import datetime
 
 # Webhook Config
-webhook = DiscordWebhook(url=discord_webhook_url, avatar_url="https://i.imgur.com/IpIG5TP.png", username="Instagram Statistics Tracker")
+webhook = DiscordWebhook(url=discord_webhook_url, avatar_url="https://i.imgur.com/TBC.png", username="NC Chase Logging")
 
-embed = DiscordEmbed(title="Initialised", description="Initialised at " + init_time_with_day, color=0xFEFEFE)
-embed.set_footer(text=version)
-embed.add_embed_field(name="Tracking ", value=scrape_username, inline=False)
-webhook.add_embed(embed) # Add embed object to webhook
-response = webhook.execute() # Send Webhook
-webhook.remove_embed(0)
+def initialised(script):
+    now = datetime.now()
+    time = now.strftime("%d-%m-%Y %H:%M:%S")
+    embed = DiscordEmbed(title="Initialised " + script, description="Initialised at " + time, color=0xFEFEFE)
+    # embed.set_footer(text=version)
+    # embed.set_timestamp()
+    embed.add_embed_field(name="Tracking ", value="TBC", inline=False)
+    webhook.add_embed(embed) # Add embed object to webhook
+    response = webhook.execute() # Send Webhook
+    webhook.remove_embed(0)
+
+def report():
+    embed = DiscordEmbed(title="TITLE", description="description", color="", )
+    embed.set_timestamp
+
+
+
