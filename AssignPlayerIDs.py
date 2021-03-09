503e6ab2f4bbf5be9f4a82b6f441ffe25c4d9e54
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-03-10 11:25:24 +1300

    Fix Assign ID Script (UNTESTED)

--- AssignPlayerIDs.py
+++ AssignPlayerIDs.py
@@ -9,8 +9,8 @@ numberOfy12 = numberOfy11 + 0
 numberOfy13 = numberOfy12 + 0
 
 def AssignPlayerIDs():
-    gc = gspread.service_account(filename=service_account_filepath)
-    sh = gc.open_by_key(spreadsheetkey)
+    gc = gspread.service_account(filename=service_account_9_filepath)
+    sh = gc.open_by_key(spreadsheet_key)
     YEAR9 = sh.worksheet("YEAR9")
     YEAR10 = sh.worksheet("YEAR10")
     YEAR11 = sh.worksheet("YEAR11")
@@ -27,26 +27,6 @@ def AssignPlayerIDs():
     YEAR11.update("A2:A{}".format(numberOfy11 + 1), allID[numberOfy10:numberOfy11])
     YEAR12.update("A2:A{}".format(numberOfy12 + 1), allID[numberOfy11:numberOfy12])
     YEAR13.update("A2:A{}".format(numberOfy13 + 1), allID[numberOfy13:numberOfy9])
-
-    # elif year == 10:
-    #     gc = gspread.service_account(filename=service_account_10_filepath)
-    #     sh = gc.open_by_key(spreadsheet_key)
-    #     worksheet = sh.worksheet("YEAR10")
-
-    # elif year == 11:
-    #     gc = gspread.service_account(filename=service_account_11_filepath)
-    #     sh = gc.open_by_key(spreadsheet_key)
-    #     worksheet = sh.worksheet("YEAR11")
-
-    # elif year == 12:
-    #     gc = gspread.service_account(filename=service_account_12_filepath)
-    #     sh = gc.open_by_key(spreadsheet_key)
-    #     worksheet = sh.worksheet("YEAR12")
-
-    # elif year == 13:
-    #     gc = gspread.service_account(filename=service_account_13_filepath)
-    #     sh = gc.open_by_key(spreadsheet_key)
-    #     worksheet = sh.worksheet("YEAR13")  
 
 confirmation = input("Are you sure you want to assign Player IDs? This will overwrite the existing IDs.\nTHIS IS IRREVERSIBLE\nYES or NO\n")
 
