0de318f9e73bbe68aca3041e74523cb12f2ffd6b
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-03-19 23:39:13 +1300

    Split email files into multiple for ease of CLI usage

---
+++ EmailPunish.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def PunishmentEmail(ID):
+    # Signing into service account & allowing access
+
+    """ SOMEHOW GET INFO FROM ID"""
+
+
+
+if __name__ == '__main__':
+    PunishmentEmail()
+
+# NOTE USE sys arguments for ID etc? parse them?
