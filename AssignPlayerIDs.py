bac6bbe3133b75cd0d9edc01359097680dcb559d
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-03-19 21:40:36 +1300

    New ID creation system

--- AssignPlayerIDs.py
+++ AssignPlayerIDs.py
@@ -17,33 +17,47 @@ def AssignPlayerIDs():
     NOY11 = len(YEAR11.col_values(2)) - 1
     NOY12 = len(YEAR12.col_values(2)) - 1
     NOY13 = len(YEAR13.col_values(2)) - 1
-    
-    # # Uncomment for Testing.
-    # NOY9 = 80
-    # NOY10 = 80
-    # NOY11 = 80
-    # NOY12 = 80
-    # NOY13 = 80
 
     pre = 0
-    As = [["A{}".format(i)] for i in range(101, 500)]
-    Bs = [["B{}".format(i)] for i in range(101, 500)]
-    Cs = [["C{}".format(i)] for i in range(101, 500)]
-    Ds = [["D{}".format(i)] for i in range(101, 500)]
-    allID = As + Bs + Cs + Ds
-    shuffle(allID)
-    YEAR9.update("A2:A{}".format(NOY9 + 1), allID[:NOY9])
+    list_of_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "K", "M", "R", "T", "X"]
+    all_id_list = []
+
+    for letter in list_of_letters:
+        new_letter_list = [[letter + "{}".format(i)] for i in range(101, 500)]
+        all_id_list += new_letter_list
+    
+    # Bs = [["B{}".format(i)] for i in range(101, 500)]
+    # Cs = [["C{}".format(i)] for i in range(101, 500)]
+    # Ds = [["D{}".format(i)] for i in range(101, 500)]
+    # Es = [["E{}".format(i)] for i in range(101, 500)]
+    # Fs = [["F{}".format(i)] for i in range(101, 500)]
+    # Gs = [["G{}".format(i)] for i in range(101, 500)]
+    # Hs = [["H{}".format(i)] for i in range(101, 500)]
+    # # I, J skipped
+    # Ks = [["K{}".format(i)] for i in range(101, 500)]
+    # # L skipped
+    # Ms = [["M{}".format(i)] for i in range(101, 500)]
+    # # N, O, P, Q skipped
+    # Rs = [["R{}".format(i)] for i in range(101, 500)]
+    # # S skipped
+    # Ts = [["T{}".format(i)] for i in range(101, 500)]
+    # # U, V, W skipped
+    # Xs = [["X{}".format(i)] for i in range(101, 500)]
+    # allID = As + Bs + Cs + Ds + Es + Fs + Gs
+
+    shuffle(all_id_list)
+    
+    YEAR9.update("A2:A{}".format(NOY9 + 1), all_id_list[:NOY9])
     pre += NOY9
-    YEAR10.update("A2:A{}".format(NOY10 + 1), allID[pre:NOY10 + pre])
+    YEAR10.update("A2:A{}".format(NOY10 + 1), all_id_list[pre:NOY10 + pre])
     pre += NOY10
-    YEAR11.update("A2:A{}".format(NOY11 + 1), allID[pre:NOY11 + pre])
+    YEAR11.update("A2:A{}".format(NOY11 + 1), all_id_list[pre:NOY11 + pre])
     pre += NOY11
-    YEAR12.update("A2:A{}".format(NOY12 + 1), allID[pre:NOY12 + pre])
+    YEAR12.update("A2:A{}".format(NOY12 + 1), all_id_list[pre:NOY12 + pre])
     pre += NOY12
-    YEAR13.update("A2:A{}".format(NOY13 + 1), allID[pre:NOY13 + pre])
+    YEAR13.update("A2:A{}".format(NOY13 + 1), all_id_list[pre:NOY13 + pre])
 
     
-# AssignPlayerIDs()
 confirmation = input("Are you sure you want to assign Player IDs? This will overwrite the existing IDs.\nTHIS IS IRREVERSIBLE\nYES or NO\n")
 if confirmation == "YES":
     AssignPlayerIDs()
