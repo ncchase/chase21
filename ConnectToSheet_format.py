6e6529305ceab5c9ca1017a7936ac049a6e830d6
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-03-21 21:01:26 +1300

    Add format to access spreadsheet via random svc acc

--- ConnectToSheet_format.py
+++ ConnectToSheet_format.py
@@ -1,5 +1,6 @@
 import gspread
-from example_credentials import *
+from credentials import *
+import random
 
 def EXAMPLE(year):
     if year == 9:
@@ -27,3 +28,18 @@ def EXAMPLE(year):
         sh = gc.open_by_key(spreadsheet_key)
         worksheet = sh.worksheet("YEAR13")  
 
+# Accessing spreadsheet using random service account (keeps read limit load relatively even)
+# NOTE: import random
+service_acc_num = random.randint(1,5)
+if service_acc_num == 1:
+    gc = gspread.service_account(filename=service_account_9_filepath)
+elif service_acc_num == 2:
+    gc = gspread.service_account(filename=service_account_10_filepath)
+elif service_acc_num == 3:
+    gc = gspread.service_account(filename=service_account_11_filepath)
+elif service_acc_num == 4:
+    gc = gspread.service_account(filename=service_account_12_filepath)
+elif service_acc_num == 5:
+    gc = gspread.service_account(filename=service_account_13_filepath)
+sh = gc.open_by_key(spreadsheet_key)
+    
