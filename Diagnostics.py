03f01843a75945da99b9291f769636a5182d7362
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-03-23 21:13:33 +1300

    Prepare Diagnostics

--- Diagnostics.py
+++ Diagnostics.py
@@ -1,3 +1,9 @@
 import RetrievePlayerInfo
 import gspread
 import Logging
+
+# SCRIPT LOW PRIORITY
+
+# Check for reciprocal runners (two ppl chasing each other). See https://github.com/ncchase/NC-Chase/blob/master/TargetEachFind.py
+# Check for un-chased people. See https://github.com/ncchase/NC-Chase/blob/master/NonTargets.py
+# Check for people with more than 1 chaser. See https://github.com/ncchase/NC-Chase/blob/master/MostCommon.py
