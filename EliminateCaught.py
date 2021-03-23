c0b603d3a303b74dba2bbb3bb5a63b200afcee82
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-03-23 21:25:41 +1300

    Fix blankline

--- EliminateCaught.py
+++ EliminateCaught.py
@@ -1,3 +1,4 @@
 # This script iterates through the caught form, and eliminates players who have been caught. It also validates etcetc.
 
 # 2020 version of this script at https://github.com/ncchase/NC-Chase/blob/master/RemovePlayersV3.py
+    
