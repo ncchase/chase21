203975ba39e9299e4be11d0cac00a036064f8142
coconuts <77216874+WristWash@users.noreply.github.com>
2021-03-25 21:51:27 +1300

    Update EliminateCaught.py
    First steps making eliminate players program. 2/7 Planned steps complete. Next: Validation of IDs (easy), parsing of ID's and movement of data into correct/relevant sheets (not so easy)

--- EliminateCaught.py
+++ EliminateCaught.py
@@ -2,3 +2,93 @@
 
 # 2020 version of this script at https://github.com/ncchase/NC-Chase/blob/master/RemovePlayersV3.py
     
+import gspread
+from credentials import *
+import random
+import numpy as np
+import logging
+
+def Eliminate():
+
+        service_acc_num = random.randint(1,4)
+        if service_acc_num == 1:
+            gc = gspread.service_account(filename=service_account_9_filepath)
+        elif service_acc_num == 2:
+            gc = gspread.service_account(filename=service_account_10_filepath)
+        elif service_acc_num == 3:
+            gc = gspread.service_account(filename=service_account_11_filepath)
+        elif service_acc_num == 4:
+            gc = gspread.service_account(filename=service_account_12_filepath)
+        sh = gc.open_by_key(spreadsheet_key)
+
+        # Importing data of ALL year groups + removed players sheets + caught form sheets + caught form removed sheets
+        
+        sh = gc.open_by_key(spreadsheet_key)
+        gc = gspread.service_account(filename=service_account_9_filepath)
+        Y9Sheet = sh.worksheet("YEAR9")
+        Y9 = np.array(Y9Sheet.get_all_values())
+
+        gc = gspread.service_account(filename=service_account_9_filepath)
+        Y9RSheet = sh.worksheet("YEAR9_REMOVED")
+        Y9R = np.array(Y9RSheet.get_all_values())
+
+        gc = gspread.service_account(filename=service_account_10_filepath)
+        Y10Sheet = sh.worksheet("YEAR10")
+        Y10 = np.array(Y10Sheet.get_all_values())
+
+        gc = gspread.service_account(filename=service_account_9_filepath)
+        Y10R = sh.worksheet("YEAR10_REMOVED")
+        Y10 = np.array(Y10R.get_all_values())
+
+        gc = gspread.service_account(filename=service_account_11_filepath)
+        Y11Sheet = sh.worksheet("YEAR11")
+        Y11 = np.array(Y11Sheet.get_all_values())
+
+        gc = gspread.service_account(filename=service_account_9_filepath)
+        Y11RSheet = sh.worksheet("YEAR11_REMOVED")
+        Y11R = np.array(Y11RSheet.get_all_values())
+
+        gc = gspread.service_account(filename=service_account_12_filepath)
+        Y12Sheet = sh.worksheet("YEAR12")
+        Y12 = np.array(Y12Sheet.get_all_values())
+
+        gc = gspread.service_account(filename=service_account_9_filepath)
+        Y12RSheet = sh.worksheet("YEAR12_REMOVED")
+        Y12R = np.array(Y12RSheet.get_all_values())
+
+        gc = gspread.service_account(filename=service_account_9_filepath)
+        FCSheet = sh.worksheet("FORM_CAUGHT")
+        FC = np.array(FCSheet.get_all_values())
+
+        gc = gspread.service_account(filename=service_account_9_filepath)
+        FCRSheet = sh.worksheet("FORM_CAUGHT_REMOVED")
+        FCR = np.array(FCRSheet.get_all_values())
+
+        # Creating lists of Player:Target pairs to validate against
+        # PTL = Player Target List
+
+        Y9PTL = [[player, target] for player, target in zip(list(Y9[:,0]), list(Y9[:,6]))]
+        Y10PTL = [[player, target] for player, target in zip(list(Y10[:,0]), list(Y10[:,6]))]
+        Y11PTL = [[player, target] for player, target in zip(list(Y11[:,0]), list(Y11[:,6]))]
+        Y12PTL = [[player, target] for player, target in zip(list(Y12[:,0]), list(Y12[:,6]))]
+
+        # Removing headings 
+        del Y9PTL[0], Y10PTL[0], Y11PTL[0], Y12PTL[0]
+
+        # Creating main PTL list
+        PTL = Y9PTL + Y10PTL + Y11PTL + Y12PTL
+        
+        # print(PTL)
+        # print(len(PTL))
+        
+        CF_PTL = [[chaser, runner] for chaser, runner in zip(list(FC[:,2]), list(FC[:,3]))]
+        del CF_PTL[0]
+        
+
+        # Iterating through entries in the CF Sheet
+        for pair_num in range(1,len(PTL) + 1):
+            print(pair_num, "hello")
+            if CF_PTL[pair_num] in PTL:
+                print("This is a real pair")
+
+Eliminate()
