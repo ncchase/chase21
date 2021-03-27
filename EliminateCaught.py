b89dded99ff6074532fc2bbf86f53205ebfaf4ed
coconuts <77216874+WristWash@users.noreply.github.com>
2021-03-27 21:47:06 +1300

    Actually removes players from sheet now
    Year sheets will now show caught points and will remove runners from the sheet (adding players to removed sheet to come)

--- EliminateCaught.py
+++ EliminateCaught.py
@@ -18,34 +18,34 @@ def Eliminate():
 
     sh = gc.open_by_key(spreadsheet_key)
     Y9Sheet = sh.worksheet("YEAR9")
-    Y9 = np.array((Y9Sheet.get_all_values()))
+    Y9 = np.array((Y9Sheet.get_all_values()[1:]))
 
     Y9RSheet = sh.worksheet("YEAR9_REMOVED")
-    Y9R = np.array(Y9RSheet.get_all_values())
+    Y9R = np.array(Y9RSheet.get_all_values()[1:])
 
     Y10Sheet = sh.worksheet("YEAR10")
-    Y10 = np.array(Y10Sheet.get_all_values())
+    Y10 = np.array(Y10Sheet.get_all_values()[1:])
 
     Y10RSheet = sh.worksheet("YEAR10_REMOVED")
-    Y10R = np.array(Y10RSheet.get_all_values())
+    Y10R = np.array(Y10RSheet.get_all_values()[1:])
 
     Y11Sheet = sh.worksheet("YEAR11")
-    Y11 = np.array(Y11Sheet.get_all_values())
+    Y11 = np.array(Y11Sheet.get_all_values()[1:])
 
     Y11RSheet = sh.worksheet("YEAR11_REMOVED")
-    Y11R = np.array(Y11RSheet.get_all_values())
+    Y11R = np.array(Y11RSheet.get_all_values()[1:])
 
     Y12Sheet = sh.worksheet("YEAR12")
-    Y12 = np.array(Y12Sheet.get_all_values())
+    Y12 = np.array(Y12Sheet.get_all_values()[1:])
 
     Y12RSheet = sh.worksheet("YEAR12_REMOVED")
-    Y12R = np.array(Y12RSheet.get_all_values())
+    Y12R = np.array(Y12RSheet.get_all_values()[1:])
 
     FCSheet = sh.worksheet("FORM_CAUGHT")
-    FC = np.array(FCSheet.get_all_values())
+    FC = np.array(FCSheet.get_all_values()[1:])
 
     FCRSheet = sh.worksheet("FORM_CAUGHT_REMOVED")
-    FCR = np.array(FCRSheet.get_all_values())
+    FCR = np.array(FCRSheet.get_all_values()[1:])
 
     # Creating lists of Player:Target pairs to validate against
     # PTL = Player Target List
@@ -58,8 +58,14 @@ def Eliminate():
     # Creating list of IDs for each year group
     Y9ID, Y10ID, Y11ID, Y12ID = list(Y9[:,0]), list(Y10[:,0]), list(Y11[:,0]), list(Y12[:,0])
 
+
+    # Deleting Sheet
+    print(len(Y9), "Y9LEN")
+    Y9Sheet.delete_rows(2, len(Y9)+ 1)
+
+
     # Removing headings 
-    del Y9PTL[0], Y10PTL[0], Y11PTL[0], Y12PTL[0]
+    # del Y9PTL[0], Y10PTL[0], Y11PTL[0], Y12PTL[0]
 
     # Creating main PTL list
     PTL = Y9PTL + Y10PTL + Y11PTL + Y12PTL
@@ -71,18 +77,15 @@ def Eliminate():
     
     # Creating PTL list from the cuaght form
     CF_PTL = [[chaser, runner] for chaser, runner in zip(list(FC[:,2]), list(FC[:,3]))]
-    # print(CF_PTL, "CF_PTL")
-    del CF_PTL[0]
+    print(CF_PTL, "CF_PTL")
     
     # Creating "remove" and "add point" lists
     Y9R, Y10R, Y11R, Y12R = [], [], [], []
     Y9AP, Y10AP, Y11AP, Y12AP = [], [], [], []
 
-    # print(Y9PTL, "Y9PTL")
-
     # Iterating through entries in the CF Sheet
     for pair_num in range(0,(len(CF_PTL))):
-        
+
         pair = CF_PTL[pair_num]
         chaser, runner = pair[0], pair[1]
 
@@ -107,8 +110,6 @@ def Eliminate():
             # Code for invalid ID to be added here --> logging smth smth?
             print("Invalid ID", pair)
 
-    Y9[1,11] = 111
-
     print(Y9R, Y9AP)
     print(Y10R, Y10AP)
     print(Y11R, Y11AP)
@@ -130,9 +131,32 @@ def Eliminate():
     for Y12CI in Y12AP:
         catches = int(Y12[int(Y12CI), 11])
         Y12[int(Y12CI), 11] = str(int(catches + 1))
+    
+    # Ordering deletion indexes so that deletion of one entry does not change the index of all other entries
+    Y9R.sort(reverse=True), Y10R.sort(reverse=True), Y11R.sort(reverse=True), Y12R.sort(reverse=True)
 
     # print(Y9, Y10, Y11, Y12)
+    print(Y9R, Y10R, Y11R, Y12R)
+    
 
+    # Deleting caught players from local version of sheet(s)
+    for player in Y9R:
+        print(Y9[player])
+        Y9 = np.delete(Y9, player, 0)
+    for player in Y10R:
+        Y10 = np.delete(Y10, player, 0)
+    for player in Y11R:
+        Y11 = np.delete(Y11, player, 0)
+    for player in Y12R:
+        Y12 = np.delete(Y12, player, 0)
+
+    # print(Y9)
+    print(len(Y9))
+
+    Y9Sheet.update("A2:M" + str(len(Y9) + 1), Y9.tolist())
+    Y10Sheet.update("A2:M" + str(len(Y10) + 1), Y10.tolist())
+    Y11Sheet.update("A2:M" + str(len(Y11) + 1), Y11.tolist())
+    Y12Sheet.update("A2:M" + str(len(Y12) + 1), Y12.tolist())
 
 if __name__ == "__main__":
     
