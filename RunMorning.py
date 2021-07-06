feddc82f5bf8a3fdd52712fdc7950eff954666d2
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-07-07 11:25:12 +1200

    Testing

--- RunMorning.py
+++ RunMorning.py
@@ -4,12 +4,12 @@ import Logging
 
 Logging.initialised("RunMorning.py")
 
-AssignRunner.assignRunner(9)
-AssignRunner.assignRunner(10)
-AssignRunner.assignRunner(11)
-AssignRunner.assignRunner(12)
+# AssignRunner.assignRunner(9)
+# AssignRunner.assignRunner(10)
+# AssignRunner.assignRunner(11)
+# AssignRunner.assignRunner(12)
 
 EmailRunners.EmailRunners(9)
-EmailRunners.EmailRunners(10)
-EmailRunners.EmailRunners(11)
-EmailRunners.EmailRunners(12)
+# EmailRunners.EmailRunners(10)
+# EmailRunners.EmailRunners(11)
+# EmailRunners.EmailRunners(12)
